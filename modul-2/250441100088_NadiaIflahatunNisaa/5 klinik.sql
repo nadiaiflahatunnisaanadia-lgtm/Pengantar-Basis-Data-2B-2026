96ab2809eaa865698921a33938914ed6c1b0ecbf
nadiaiflahatunnisaanadia-lgtm <nadiaiflahatunnisaanadia@gmail.com>
2026-04-09 21:50:00 +0700

    250441100088_NadiaIflahatunNisaa_Modul2_AsprakKakEdi

---
+++ modul-2/250441100088_NadiaIflahatunNisaa/5 klinik.sql
@@ -0,0 +1,35 @@
+USE db_klinik;
+
+DROP TABLE rekam_medis;
+DESCRIBE rekam_medis;
+CREATE TABLE rekam_medis (
+    id_rekam INT PRIMARY KEY,
+    id_pasien INT,
+    id_dokter INT,
+    tanggal_periksa DATE,
+    diagnosis VARCHAR(255),
+    FOREIGN KEY (id_pasien)
+        REFERENCES pasien(id_pasien)
+        ON DELETE RESTRICT,
+    FOREIGN KEY (id_dokter)
+        REFERENCES dokter(id_dokter)
+        ON DELETE RESTRICT
+);
+
+SELECT * FROM dokter;
+INSERT INTO dokter VALUES
+(1,'Dr. Andi','Umum');
+
+SELECT * FROM pasien;
+
+INSERT INTO rekam_medis
+(id_rekam,id_pasien,id_dokter,tanggal_periksa,diagnosis)
+VALUES
+(1,1,1,'2026-04-01','Demam'),
+(2,2,2,'2026-04-02','Flu');
+SELECT * FROM rekam_medis;
+
+TRUNCATE TABLE rekam_medis;
+DESCRIBE rekam_medis;
+
+TRUNCATE TABLE pasien;
