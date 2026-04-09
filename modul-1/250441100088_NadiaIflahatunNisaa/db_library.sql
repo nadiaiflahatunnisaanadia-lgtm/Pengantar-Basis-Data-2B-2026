5eae6076336e5bd557ca72a4f7d992f8ccff7fe4
nadiaiflahatunnisaanadia-lgtm <nadiaiflahatunnisaanadia@gmail.com>
2026-04-09 21:49:13 +0700

    250441100088_NadiaIflahatunNisaa_Modul1_AsprakKakEdi

---
+++ modul-1/250441100088_NadiaIflahatunNisaa/db_library.sql
@@ -0,0 +1,42 @@
+USE db_library;
+
+
+CREATE TABLE koleksi_langka (
+    id_buku BIGINT PRIMARY KEY,
+    judul VARCHAR(250),
+    tahun_terbit YEAR,
+    estimasi_harga DECIMAL(15,2)
+);
+
+DESCRIBE koleksi_langka;
+
+CREATE TABLE pelanggan_setia (
+    nomor_identitas INT
+);
+
+ALTER TABLE pelanggan_setia
+MODIFY nomor_identitas VARCHAR(20);
+
+SHOW COLUMNS FROM pelanggan_setia;
+
+CREATE TABLE transaksi_harian (
+    id_transaksi INT,
+    total_transaksi DECIMAL(10,2)
+);
+
+DROP TABLE transaksi_harian;
+
+SHOW COLUMNS FROM transaksi_harian;
+
+CREATE TABLE umpan_balik (
+    id_komentar INT AUTO_INCREMENT PRIMARY KEY,
+    nama_pengguna VARCHAR(100),
+    isi_komentar TEXT
+);
+
+DESCRIBE umpan_balik;
+
+
+
+
+
